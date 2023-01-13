30fdf9efe5489d38f9baa1056714757808f105cf
nick <nick.salvetti@live.com>
2023-01-12 18:11:09 -0600

    first

---
+++ taxes.test.js
@@ -0,0 +1,24 @@
+describe('calculate taxes', () => {
+   it('should calculate low-bracket', function () {
+      expect(calculateTaxes(10000)).toEqual(1500);
+      expect(calculateTaxes(20000)).toEqual(3000);
+   });
+   it('should calculate high-bracket', function () {
+      expect(calculateTaxes(50000)).toEqual(12500);
+      expect(calculateTaxes(80000)).toEqual(20000);
+   });
+})
+
+describe('remove duplicates', () => {
+   it('should remove duplicates from an array', () => {
+      expect(() => {
+         calculateTaxes('askdalkdj')
+      }).toThrowError()
+      expect(() => {
+         calculateTaxes([])
+      }).toThrowError()
+      expect(() => {
+         calculateTaxes(true)
+      }).toThrowError()
+   })
+})
